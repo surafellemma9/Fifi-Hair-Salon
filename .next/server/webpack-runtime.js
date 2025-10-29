8fe7ba998144d09bb1000f02bb3d61a82fa87ba7
surafellemma9 <surafellemma9@gmail.com>
2025-10-28 23:50:15 -0400

    fixing render issue

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f7ec6c1f5fe3cc2b")
+/******/ 		__webpack_require__.h = () => ("0b1b6df1c88f3d8a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "19ffb4cc6db7bb12"; }
+/******/ 		__webpack_require__.h = function() { return "c0f54acb517dd7d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
